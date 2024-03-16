82597335dd2df71ee6b4f1b89e35ee60961b6409
mimoreir <miguelmontenegrosilva@hotmail.com>
2024-03-16 15:05:58 +0000

    GNL

---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimoreir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 09:28:10 by mimoreir          #+#    #+#             */
+/*   Updated: 2022/12/02 09:28:12 by mimoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*initsave(char *save);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimoreir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 09:28:44 by mimoreir          #+#    #+#             */
+/*   Updated: 2022/12/02 09:28:45 by mimoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*tmp;
+	char	a;
+
+	tmp = (char *)s;
+	a = (char)c;
+	while (*tmp)
+	{
+		if (*tmp == a)
+			return (tmp);
+		tmp++;
+	}
+	if (*tmp == '\0' && a == '\0')
+		return (tmp);
+	else
+		return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new;
+	char	*aux;
+	size_t	len1;
+	size_t	len2;
+
+	len1 = ft_strlen((char *)s1);
+	len2 = ft_strlen((char *)s2);
+	new = (char *)malloc(len1 + len2 + 1);
+	if (!new)
+		return (NULL);
+	aux = new;
+	ft_memcpy(aux, s1, len1);
+	aux = aux + len1;
+	ft_memcpy(aux, s2, len2);
+	aux = aux + len2;
+	*aux = '\0';
+	return (new);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s)
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+char	*initsave(char *save)
+{
+	if (!save)
+	{
+		save = malloc(1 * sizeof(char));
+		*save = '\0';
+	}
+	return (save);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char		*a;
+	const char	*b;
+	size_t		i;
+
+	i = 0;
+	a = dest;
+	b = src;
+	if (src == dest)
+		return (dest);
+	while (i < n)
+	{
+		*a = *b;
+		a++;
+		b++;
+		i++;
+	}
+	return (dest);
+}
